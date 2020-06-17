d5da444b2b2a92b26c9b9611e4b4e7a95abc346c
IainHarrison <59735994+IainHarrison@users.noreply.github.com>
2020-06-17 21:58:19 +0200

    Update Boid.cs

--- Boids/Assets/Scripts/Boid.cs
+++ Boids/Assets/Scripts/Boid.cs
@@ -9,16 +9,16 @@ public class Boid : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        velocity = this.transform.forward * maxVelocity;
+        Velocity = this.transform.forward * MaxVelocity;
     }
 
     // Update is called once per frame
     void Update()
     {
         if ( velocity.magnitude > maxVelocity)
-            velocity = velocity.normalized * maxVelocity;
+            Velocity = Velocity.normalized * MaxVelocity;
 
-        this.transform.position += velocity * Time.deltaTime; //move 
+        this.transform.position += Velocity * Time.deltaTime; //move 
         this.transform.rotation = Quaternion.LookRotation(velocity); //look in direction of movment
     }
 }
